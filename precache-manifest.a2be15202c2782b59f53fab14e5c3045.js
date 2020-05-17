7367e682e242ff74253515dbb8120785770a0a67
39Productions <ken@39productions.com>
2020-05-17 20:38:57 +0100

    Updates

---
+++ precache-manifest.a2be15202c2782b59f53fab14e5c3045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "500d17178c8e129f4239433c26ab0afe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d8619cf73fb82c4c8914",
+    "url": "/monsters-rolodex/static/css/main.323e45df.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8619cf73fb82c4c8914",
+    "url": "/monsters-rolodex/static/js/main.a435ecda.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
